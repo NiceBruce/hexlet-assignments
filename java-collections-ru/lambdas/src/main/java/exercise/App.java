bd9db63be1d4f6a0197640173bd0f9bbbc2d1e37
@hexlet/cli <support@hexlet.io>
2022-11-14 14:38:19 +0300

    submit java-collections-ru/lambdas

--- java-collections-ru/lambdas/src/main/java/exercise/App.java
+++ java-collections-ru/lambdas/src/main/java/exercise/App.java
@@ -1,56 +1,36 @@
 package exercise;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 // BEGIN
 class App {
     public static String[][] enlargeArrayImage(String[][] image) {
 
-        int lengthOfImage = (image.length != 0) ? image.length * 2 : 0;
-        String [][] res = new String[lengthOfImage][];
-        int count = 0;
-
-        for (int i = 1; i < res.length; i+=2) {
-            res[i] = new String[image[count].length * 2];
-
-            for (int j = 0; j < image.length; j++) {
-
-                res[i][j+j] = image[count][j];
-                res[i][j+j+1] = image[count][j];
-            }
-
-            res[i - 1] = Arrays.copyOf(res[i], res[i].length);
-            count++;
-        }
-
-        return res;
-    }
-
-    public static void main(String[] args) {
-
-        String[][] image = {
-                {"*", "*", "*", "*"},
-                {"*", " ", " ", "*"},
-                {"*", " ", " ", "*"},
-                {"*", "*", "*", "*"},
-        };
-
-//        String[][] res = Arrays.stream(image).map(String[] :: clone).toArray(String[][] :: new);
-
-//        Arrays.stream(image).flatMap(x -> Arrays.stream(x)).forEach(x -> System.out.println(x));
-//        Arrays.stream(image).flatMap(x -> Arrays.stream(x)).forEach(System.out::print);
-
-//        for (String[] e : res) {
-//            System.out.println(e);
+//        int lengthOfImage = (image.length != 0) ? image.length * 2 : 0;
+//        String [][] res = new String[lengthOfImage][];
+//        int count = 0;
+//
+//        for (int i = 1; i < res.length; i+=2) {
+//            res[i] = new String[image[count].length * 2];
+//
+//            for (int j = 0; j < image.length; j++) {
+//
+//                res[i][j+j] = image[count][j];
+//                res[i][j+j+1] = image[count][j];
+//            }
+//
+//            res[i - 1] = Arrays.copyOf(res[i], res[i].length);
+//            count++;
 //        }
-        String[][] res = App.enlargeArrayImage(image);
-        Arrays.stream(image).map(i -> i[3] + i[3]).forEach(System.out::print);
-        System.out.println();
-        Arrays.stream(res).flatMap(Arrays::stream).forEach(System.out::print);
-//        System.out.println(Arrays.deepToString(res));
+
+        return Arrays.stream(image)
+                .map(innerStr -> {
+                    return Arrays.stream(innerStr)
+                            .flatMap(g -> Stream.of(g,g)).toArray(String[] :: new);
+                })
+                .flatMap(outerStr -> Stream.of(outerStr,outerStr))
+                .toArray(String[][] :: new);
     }
 }
 // END
--- java-collections-ru/lambdas/src/test/java/exercise/AppTest.java
+++ java-collections-ru/lambdas/src/test/java/exercise/AppTest.java
@@ -4,5 +4,56 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 // BEGIN
+class AppTest {
+    @Test
+    void testApp() {
+        String[][] image = {
+                {"*", "*", "*", "*"},
+                {"*", " ", " ", "*"},
+                {"*", " ", " ", "*"},
+                {"*", "*", "*", "*"},
+        };
 
+        String[][] actual = App.enlargeArrayImage(image);
+
+        String[][] expected = {
+                {"*", "*", "*", "*","*", "*", "*", "*"},
+                {"*", "*", "*", "*","*", "*", "*", "*"},
+                {"*", "*", " ", " ", " ", " ", "*", "*"},
+                {"*", "*", " ", " ", " ", " ", "*", "*"},
+                {"*", "*", " ", " ", " ", " ", "*", "*"},
+                {"*", "*", " ", " ", " ", " ", "*", "*"},
+                {"*", "*", "*", "*","*", "*", "*", "*"},
+                {"*", "*", "*", "*","*", "*", "*", "*"},
+        };
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testAppEmpty() {
+        String[][] image = {};
+
+        String[][] actual = App.enlargeArrayImage(image);
+        String[][] expected = {};
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+
+    @Test
+    void testAppWitnOneElement() {
+        String[][] image = {
+                {"*"},
+        };
+
+        String[][] actual = App.enlargeArrayImage(image);
+
+        String[][] expected = {
+                {"*", "*"},
+                {"*", "*"},
+
+        };
+        assertThat(actual).isEqualTo(expected);
+    }
+}
 // END
