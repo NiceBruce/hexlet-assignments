f9e9040745025e63103b7a66c3a4ebac4c3c9664
@hexlet/cli <support@hexlet.io>
2022-11-16 23:51:33 +0300

    submit java-collections-ru/other-collections

---
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -0,0 +1,37 @@
+package exercise;
+
+import java.sql.SQLOutput;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+// BEGIN
+class App {
+    public static LinkedHashMap<String, String> genDiff(Map<String, Object> data1, Map<String, Object> data2) {
+
+        Map<String, String> result = new TreeMap<String, String>();
+
+        for (var e : data2.entrySet()) {
+            if (data1.containsKey(e.getKey())) {
+                if (data1.get(e.getKey()).equals(e.getValue())) {
+                    result.put(e.getKey(), "unchanged");
+                } else {
+                    result.put(e.getKey(), "changed");
+                }
+            } else {
+                result.put(e.getKey(), "added");
+            }
+        }
+
+        for (var e : data1.entrySet()) {
+            if (!data2.containsKey(e.getKey())) {
+                result.put(e.getKey(), "deleted");
+            }
+        }
+
+        LinkedHashMap<String, String> sortedMap = new LinkedHashMap<>(result);
+
+        return  sortedMap;
+    }
+}
+//END
