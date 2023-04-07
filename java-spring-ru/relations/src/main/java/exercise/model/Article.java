149608297af790c87e456ef5fce84f7ee0534133
@hexlet/cli <support@hexlet.io>
2023-04-07 23:12:16 +0300

    submit java-spring-ru/relations

---
+++ java-spring-ru/relations/src/main/java/exercise/model/Article.java
@@ -0,0 +1,33 @@
+package exercise.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.GenerationType;
+
+// BEGIN
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    @Lob
+    private String body;
+
+    @ManyToOne
+    private Category category;
+}
+// END
